1dd916bb25697f24eeb5e701824983975dc3f049
Rick Kim <kim.rick@gmail.com>
2025-03-17 11:36:56 -0700

    feat: scaffold Vite + vue-ts + tailwindcss project in /client

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+defineProps<{ msg: string }>()
+
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    {{ msg }}
+  </h1>
+</template>
+
+<style scoped>
+</style>
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RallyModule } from './rally/rally.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client/dist'),
+    }),
     MongooseModule.forRoot('mongodb://localhost/goShuttle'),
     RallyModule,
   ],
